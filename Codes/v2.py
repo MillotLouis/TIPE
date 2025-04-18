ef48f34ca64293bb9275d3961a966beba65e0619
Louis-m2710 <millotlouis13@gmail.com>
2025-04-18 15:25:00 +0200

    TIPE

--- Codes/v2.py
+++ Codes/v2.py
@@ -10,8 +10,8 @@ class NetworkSimulator:
         self.G = nx.DiGraph()
         self.routing_algorithm = nx.dijkstra_path  # Algorithme par défaut
         
-    def add_node(self, node_id):
-        self.G.add_node(node_id)
+    def add_node(self, node_id,x,y,bat):
+        self.G.add_node((node_id,{"pos":[x,y],"bat":bat}))
         
     def add_link(self, src, dest, weight=1, capacity=1):
         self.G.add_edge(src, dest, weight=weight, capacity=capacity)
@@ -24,8 +24,13 @@ class NetworkSimulator:
         path = self.routing_algorithm(self.G, src, dest, weight='weight')
         return self.env.process(self._transmit_packet(path, payload))
     
+    def dist(self,node1,node2):
+        return ((self.G.nodes[node2]["pos"][0] - self.G.nodes[node1]["pos"][0])**2 + (self.G.nodes[node2]["pos"][1] - self.G.nodes[node1]["pos"][1])**2)**0.5
+    
+    
     def _transmit_packet(self, path, payload):
         """Simule la transmission du paquet à travers le chemin"""
+
         
 
 # Exemple d'utilisation
